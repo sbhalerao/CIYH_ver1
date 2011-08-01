079f5033779e583022f04c4e92830c0ed4a53151
sbhalerao <sbhalerao@gmail.com>
2011-07-31 18:12:44 -0700

    added migration for user token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110729045429) do
+ActiveRecord::Schema.define(:version => 20110801011124) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20110729045429) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "token"
   end
 
   create_table "votes", :force => true do |t|
