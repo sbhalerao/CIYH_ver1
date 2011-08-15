3211fe8c667e5c3404e70ae105eab503575e4aa4
sbhalerao <sbhalerao@gmail.com>
2011-08-15 16:02:25 -0700

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-    $("#select_link").click(function(){
+    $("#jmsg").click(function(){
        $.ajax({
         type: 'POST',
         url: 'http://your_url/jmsg',
