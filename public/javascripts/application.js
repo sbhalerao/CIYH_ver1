e3c8769c976993cff90766bf896eef20d585c40c
sbhalerao <sbhalerao@gmail.com>
2011-08-15 16:10:13 -0700

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-    $("#jmsg").click(function(){
+    $("#messageid").click(function(){
        $.ajax({
         type: 'POST',
         url: 'http://high-robot-603.heroku.com/jmsg',
