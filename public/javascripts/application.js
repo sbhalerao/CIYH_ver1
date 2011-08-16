3c2bb8bd369c947d1d1aab76209efc4c9329d805
sbhalerao <sbhalerao@gmail.com>
2011-08-16 07:42:07 -0700

    json

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$.ajaxSetup({
+  'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript") }
+});
 $(function(){
     $("#messageid").click(function(){
       alert ("Hello world"); 
