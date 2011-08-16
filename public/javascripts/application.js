d142da28c488d1dcd0f0859cc4baacdb76e9a42b
sbhalerao <sbhalerao@gmail.com>
2011-08-15 22:32:20 -0700

    js debug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
     $("#messageid").click(function(){
-      alert ("Hello world") 
+      alert ("Hello world"); 
       $.ajax({
         type: 'POST',
         url: 'http://high-robot-603.heroku.com/jmsg',
