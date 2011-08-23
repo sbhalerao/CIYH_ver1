068aec33baa30ca321961857b63a9378b63bce3b
sbhalerao <sbhalerao@gmail.com>
2011-08-22 21:59:09 -0700

    amazon s3 addition

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ActiveRecord::Base
  
-  has_attached_file :avatar, :url  => "/avatars/:id/:style/:basename.:extension",
-    :path => ":rails_root/public/avatars/:id/:style/:basename.:extension"
+  has_attached_file :avatar, 
+   :storage => :s3,
+       :bucket => 'catsinyourhats',
+       :s3_credentials => {
+         :access_key_id => ENV['S3_KEY'],
+         :secret_access_key => ENV['S3_SECRET']
+       }
   
 end
