069f6848dbf21449dd52fbf35d5df55b8163a376
sbhalerao <sbhalerao@gmail.com>
2011-08-26 07:36:52 -0700

    avatar trial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.xml
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new(params[:post], :avatar => "/images/cute-kitten.jpg")
 
     respond_to do |format|
       if @post.save
