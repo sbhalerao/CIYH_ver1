ec8cadd9355912e40a9accf5329f2b0653abe786
sbhalerao <sbhalerao@gmail.com>
2011-08-29 16:42:51 -0700

    rmagick and heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   def create
     tmpfile=Tempfile.new('my_pic')
      
-      images=ImageList.new ("#{RAILS_ROOT}/public/images/cute-kitten.jpg")
+      images=ImageList.new ("#{RAILS_ROOT}/images/cute-kitten.jpg")
       images.write(tmpfile.path)
     @post = Post.new(params[:avatar =>tmpfile])
     # @post.save
