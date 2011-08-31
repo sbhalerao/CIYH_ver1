6894a0754429b23ccc26080a27aee03a7dd1fa9e
sbhalerao <sbhalerao@gmail.com>
2011-08-31 14:05:41 -0700

    json parsing in ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,9 +22,10 @@ $(function(){
       $.ajax({
         type: 'POST',
         url: 'http://high-robot-603.heroku.com/jmsg',
-		dataType: 'json',
+		// dataType: 'json',
         data: { msg: "hello world"},
-		success: function(json, status, xhr){ alert ('Success')
+		success: function(json, status, xhr)
+		{ alert ('Success')
 	   //$("#show_message").html("hello world");
 	 	},
 	    error: function(data, status, xhr){if(status=='parsererror') {alert('Error')}; }
