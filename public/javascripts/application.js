bb7903d949a646ec3a4dac9653cb7494fa1d8ed0
sbhalerao <sbhalerao@gmail.com>
2011-08-30 22:58:33 -0700

    js post

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-/*
+//$.ajaxSetup provides the default settings for $.ajax()
+// beforeSend - pre-request callback used to modify jqXHR (XMLHTTPRequest). Returning false in this function cancels the request.
+// data - data to be sent to server, converted to a questy string. Object is key:value pair
+/*success - function to be called with the request succeeds. data - data retunred from the server (formatted according
+to dataType parameter), string describing statys and the jqXHR (XMLHTTPRequest object). In the code below,
+xhr is used to specify jqXHR
+
+jqXHR object- jQuery XMLHTTPRequest is superset of XMLHTTPRequest. It contains- responseTest, responseXML, getResponseHeader() method.
+The XMLHttpRequest object is used to exchange data with a server behind the scenes.
+
+*/
+
 
 $(function(){
 	$.ajaxSetup({
@@ -20,6 +31,3 @@ $(function(){
    
      });
 });
-*/
-
-var $cur= "hello world";
