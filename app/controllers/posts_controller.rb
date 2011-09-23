19effd3a8cc1108afaece8b9ecf34cb57e46fabb
sbhalerao <sbhalerao@gmail.com>
2011-09-23 10:55:46 -0700

    right size

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,8 +76,8 @@ class PostsController < ApplicationController
    cat2= "#{RAILS_ROOT}/public/images/gloria2.jpg"
    images=ImageList.new(cat1, cat2)
    # images[1].page=Rectangle.new(images[1].columns, images[1].rows, @post.cat1x, @post.cat1y)
-   images[1]=images[1].resize_to_fit(150,110)
-   images[1].page=Rectangle.new(150, 110, @post.cat1x, @post.cat1y-100)
+   images[1]=images[1].resize_to_fit(154,118)
+   images[1].page=Rectangle.new(154, 118, @post.cat1x, @post.cat1y-90)
    com_img=images.flatten_images
    com_img.write(tmpfile.path)
  
