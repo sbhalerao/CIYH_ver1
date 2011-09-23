41c089fbbf8d97515f6b9d1f4baf3e19eeb08a22
sbhalerao <sbhalerao@gmail.com>
2011-09-22 21:22:07 -0700

    storing img co-ordinates in databse

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -96,10 +96,10 @@ class PostsController < ApplicationController
     # @message=params[:msg]
    # @message="hello world"
    @post=Post.last
-   if params[:pid]=="cat1"
+  # if params[:pid]=="cat1"
      @post.cat1x= params[:left]
      @post.cat1y=params[:top]
-   end
+  # end
      
    respond_to do |format|
      format.js
