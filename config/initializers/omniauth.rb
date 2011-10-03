46e1b19a1709ecc7caf4a56d481b578df2109233
sbhalerao <sbhalerao@gmail.com>
2011-10-02 18:49:37 -0700

    removed offline access for fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'AnSYCY4bRcmfUBJODfpmQ', 'GyvfClXeCLXL4NJBKGU7glpXDMfVDBN37pjjeQzFzw'
-  provider :facebook, '250050248346256','9735ae616f47b11f109748800d05f877', {:scope => "user_about_me,user_interests,user_likes,user_online_presence,user_photo_video_tags,user_photos,user_status,user_videos,user_website,email,read_friendlists,read_stream,publish_stream,friends_likes,friends_online_presence,friends_photo_video_tags,friends_photos"}
+  provider :facebook, '250050248346256','9735ae616f47b11f109748800d05f877', {:scope => "user_about_me,user_interests,user_likes,user_online_presence,user_photo_video_tags,user_photos,user_status,user_videos,user_website,email,read_friendlists,read_stream,publish_stream,offline_access,friends_likes,friends_online_presence,friends_photo_video_tags,friends_photos"}
 end
