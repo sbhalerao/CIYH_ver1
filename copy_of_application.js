d8ab8be461f4114befbf1d653c2e604861d9485d
sbhalerao <sbhalerao@gmail.com>
2011-10-04 22:25:58 -0700

    adding back old form display

--- copy_of_application.js
+++ copy_of_application.js
@@ -16,7 +16,7 @@ The XMLHttpRequest object is used to exchange data with a server behind the scen
 
 
     $.ajaxSetup({
-	  'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript"); }
+	  'beforeSend': function(xhr) { xhr.setRequestHeader("Accept", "text/javascript") }
 	});
 
 //sending height, width of resizable image. ID is sent as rid 	
