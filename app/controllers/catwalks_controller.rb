6f44926e94b1dcbc8848719f72443e3410596798
sbhalerao <sbhalerao@gmail.com>
2011-10-07 15:53:37 -0700

    added post your cat pics

---
+++ app/controllers/catwalks_controller.rb
@@ -0,0 +1,34 @@
+class CatwalksController < ApplicationController
+  def new
+    @catwalk=Catwalk.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @catwalk }
+    end
+  end
+
+  def create
+    @catwalk=Catwalk.new(params[:catwalk])
+    respond_to do |format|
+      if @catwalk.save
+      format.html {redirect_to (@catwalk, :notice => "picture uploaded successfully")}# new.html.erb
+      format.xml  { render :xml => @catwalk }
+    else
+      format.html {render :action => "new"}# new.html.erb
+      format.xml  { render :xml => @catwalk.errors, :status => :unprocessable_entity }
+      
+    end
+  end
+
+  def show
+    @catwalk=Catwalk.find(params[:id])
+    respond_to do |format|
+      format.html # sho.html.erb
+      format.xml  { render :xml => @catwalk }
+    end
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/models/catwalk.rb
@@ -0,0 +1,11 @@
+class Catwalk < ActiveRecord::Base
+  
+  has_attached_file :catpic, 
+   :path =>"/catpics/:id/:style/:basename.:extension",
+   :storage => :s3,
+       :bucket => 'catsinyourhats',
+       :s3_credentials => {
+         :access_key_id => ENV['S3_KEY'],
+         :secret_access_key => ENV['S3_SECRET']
+       }
+end
