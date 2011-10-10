1b638ebe7ffceedb69106b4e51ddce23a468058d
sbhalerao <sbhalerao@gmail.com>
2011-10-09 18:14:00 -0700

    index.html

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,7 @@ Blog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    
-   root :to => "front_page.html"
+ #  root :to => "front_page.html"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
