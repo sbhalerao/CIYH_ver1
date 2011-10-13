671a314587e0bc42958ff1b6ce81d77071632dab
sbhalerao <sbhalerao@gmail.com>
2011-10-12 21:37:09 -0700

    share button logic in view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -278,7 +278,7 @@ end
 def share_link
   if current_user
     respond_to do |format|
-      format.html { redirect_to(new_posts_path) }
+      format.html { redirect_to(new_post_path) }
       format.xml  { head :ok }
     end
     
