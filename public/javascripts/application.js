5aa4c17d5abbf79b2088476f799cdcf6db060f95
sbhalerao <sbhalerao@gmail.com>
2011-11-06 16:41:06 -0800

    adding javascript to get co-ordinates of the background image

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -87,7 +87,7 @@ $.ajax({
 type: 'POST',
 url: 'http://high-robot-603.heroku.com/jmsg',
 dataType: 'json',
-data:{ top: ui.offset["top"], left: ui.offset["left"], pid: (ui.draggable).attr("id"), backx:$("#droppable").offset["left"], backy:$("#droppable").offset["left"]  },
+data:{ top: ui.offset["top"], left: ui.offset["left"], pid: (ui.draggable).attr("id"), backx:$("#droppable img").offset["left"], backy:$("#droppable img").offset["left"]  },
 success: function(json, status, xhr)
 { // alert ('Success')
 //$("#show_message").html("hello world");
@@ -103,7 +103,7 @@ error: function(data, status, xhr){alert('Failure')}
 		type: 'POST',
 		url: 'http://high-robot-603.heroku.com/jmsg',
 		dataType: 'json',
-		data:{  backx:$("#droppable").offset["left"], backy:$("#droppable").offset["left"]  },
+		data:{  backx:$("#droppable img").offset["left"], backy:$("#droppable img").offset["left"]  },
 		success: function(json, status, xhr)
 		{  alert ('Success');
 		//$("#show_message").html("hello world");
