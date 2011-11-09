8bc435c64570880281519f3a00dfa2eb0bde8889
sbhalerao <sbhalerao@gmail.com>
2011-11-08 23:32:51 -0800

    pop up to view full size uploaded images in catwalk

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -153,7 +153,7 @@ error: function(data, status, xhr){alert('Failure')}
 					'type' : 'iframe'
 		   });
        
-        $("a .onesec-hor").fancybox({
+        $(".onesec-hor").fancybox({
 			'showCloseButton'	: false,
 			'titlePosition' 		: 'inside',
 			'titleFormat'		: formatTitle
