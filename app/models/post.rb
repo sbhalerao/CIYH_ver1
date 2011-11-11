02c4c4df9f11f8a45872f4c782a018357bf1d528
sbhalerao <sbhalerao@gmail.com>
2011-11-11 15:55:53 -0800

    refactoring. putting update post code in model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
          :secret_access_key => ENV['S3_SECRET']
        }
   
-  def updatePost(@post)
+  def updatePost(post)
    
     tmpfile=Tempfile.new('my_pic')
 
