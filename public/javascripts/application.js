17ca5d162897ca59833832b0f2a1a6aa755bd514
sbhalerao <sbhalerao@gmail.com>
2011-11-14 08:03:38 -0800

    testing 2 of background choices. removed all trash files

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -164,8 +164,18 @@ error: function(data, status, xhr){alert('Failure')}
 $(document).ready(function (){
 $(".background_choices img").click(function() {
   alert('Handler for .click() called.');
-  /* var filename=$(this).parent().find("img").attr("src"); */
-
+  var backimg=$(this).parent().find("img").attr("src"); 
+  	$.ajax({
+	type: 'POST',
+	url: 'http://high-robot-603.heroku.com/jmsg',
+	dataType: 'json',
+	data:{ backimg: backimg},
+	success: function(json, status, xhr)
+	{ // alert ('Success')
+	//$("#show_message").html("hello world");
+	},
+	error: function(data, status, xhr){alert('Failure')}
+	});
 
    });
 
