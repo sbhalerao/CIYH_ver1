8a8e5fd0236a2cbcfec0234124eb52d90f2cb06b
sbhalerao <sbhalerao@gmail.com>
2011-11-14 08:48:03 -0800

    replacing background image in game page after click

--- app/models/post.rb
+++ app/models/post.rb
@@ -260,7 +260,7 @@ class Post < ActiveRecord::Base
    
     
     #if background image choice is sent by user  
-     if params[:backimg]!= null
+     if params[:backimg]!= nil
         @post.update_attributes(:backimg => "#{RAILS_ROOT}/public" + params[:backimg])
      end
            
