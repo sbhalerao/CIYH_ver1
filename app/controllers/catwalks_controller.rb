47bba28bdfd2b5dff65cd3e9649d445512a83234
sbhalerao <sbhalerao@gmail.com>
2011-11-17 13:33:25 -0800

    trying out will_paginate with all catwalk images

--- app/controllers/catwalks_controller.rb
+++ app/controllers/catwalks_controller.rb
@@ -60,8 +60,8 @@ class CatwalksController < ApplicationController
  
  #paginate in descending order and do not include first 17 entries
  def index
- @catwalks=Catwalk.order('catwalks.id DESC').limit(Catwalk.last.id-17).paginate(:page => params[:page], :per_page => 16)
- 
+# @catwalks=Catwalk.order('catwalks.id DESC').limit(Catwalk.last.id-17).paginate(:page => params[:page], :per_page => 16)
+ @catwalks=Catwalk.all;
  end
  
  def authenticate
