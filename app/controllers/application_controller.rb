6964fb4abed01323f34e8b03d2e6db5f059b2f78
sbhalerao <sbhalerao@gmail.com>
2011-12-03 17:36:52 -0800

    adding 404 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
   
+  #adding customer error pages for 404 and 500 errors 
+  unless ActionController::Base.consider_all_requests_local
+      rescue_from Exception, :with => :render_error
+      rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+      rescue_from ActionController::RoutingError, :with => :render_not_found
+      rescue_from ActionController::UnknownController, :with => :render_not_found
+      rescue_from ActionController::UnknownAction, :with => :render_not_found
+  end
+  
+  
   layout "main"
   
   helper_method :current_user, :deny_access
@@ -17,5 +27,17 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]  
     end
     
+ # to render 404 and 500 error pages   
+    private
+
+      def render_not_found(exception)
+        log_error(exception)
+        render :template => "/error/404.html.erb", :status => 404
+      end
+
+      def render_error(exception)
+        log_error(exception)
+        render :template => "/error/500.html.erb", :status => 500
+      end
  
 end
