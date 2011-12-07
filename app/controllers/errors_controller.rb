2cdc1135756b89f6da9e7c1721f34dc27e600da7
sbhalerao <sbhalerao@gmail.com>
2011-12-06 21:18:25 -0800

    added comments to custom error pages code and reduced facebook permissions

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,8 +6,13 @@ class ErrorsController < ApplicationController
     :unprocessable_entity
   ].freeze
   
+  #freeze means the contents of the array cannot be changed. New stuff cannot be added etc
+  
+  
+  #Next we define three actions as included in ERRORS array. For each action, we display an error page.
+  # so we need three error pages
   ERRORS.each do |e|
-    define_method e do
+    define_method e do                    #defining dynamic method
       respond_to do |format|
         format.html { render e, :status => e }
         format.any { head e }
--- config/initializers/show_exceptions.rb
+++ config/initializers/show_exceptions.rb
@@ -1,18 +1,28 @@
 # config/initializers/show_exceptions.rb
+#modifying render_exception method from Class ShowExceptions. rails/actionpack/lib/action_dispatch/middleware/show_exceptions.rb
 require 'action_dispatch/middleware/show_exceptions'
 
+# Modifying how exceptions are handled by middleware, specifically ActionDispatch::ShowExceptions
 module ActionDispatch
   class ShowExceptions
     private
+    
+    # Rails#env returns string with current rails environment (should be production)
+    #exception is Exception object
       def render_exception_with_template(env, exception)
+        #call action in ErrorsController as though being called from routing middleware
         body = ErrorsController.action(rescue_responses[exception.class.name]).call(env)
+        #rescUe_responses is DEFAULT_RESCUE_RESONSE hash. This is defined in actionpack/lib/action_controller/rescue.rb 
+        #exception.class.name is just string name of Class of Exception
         log_error(exception)
         body
+        # If exception is thrown during call to ErrorsController, then use original method (which is aliased)
       rescue
         render_exception_without_template(env, exception)
       end
       
-      alias_method_chain :render_exception, :template
+      alias_method_chain :render_exception, :template    # Used to change behavior of :render_exception from the original ActionDispatch::ShowExceptions class
+      #Equivalent to alias_method :render_exception_without_template, :render_exception; alias_method :render_exception, :render_exception_with_template
   end
 end
 
