33b019dd6863be786eab0d361fcbb78d3e788776
sbhalerao <sbhalerao@gmail.com>
2012-01-01 11:24:04 -0800

    adding message to fb photo post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -104,6 +104,8 @@ class PostsController < ApplicationController
              FbGraph::User.me(current_user.token).photo!(
           
              :url => @post.avatar.url
+             :message => 'Made using Cats in Your Hats - http://catsinyourhats.com'
+             
             )
              
       end  
