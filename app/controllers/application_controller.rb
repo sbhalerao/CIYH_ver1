d174003be18626e5ee9f452022dc51334eb84552
sbhalerao <sbhalerao@gmail.com>
2012-01-04 00:01:23 -0800

    301 redirect www to http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :deny_access
   
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'catsinyourhats.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
+  
   
   def deny_access
      redirect_to root_path, :notice => "Please sign in"
